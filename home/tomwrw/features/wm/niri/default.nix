0da04bd135c412215cf45bba8205a2a632556185
tomwrw <tomwrw@proton.me>
2025-10-30 08:18:23 +0000

    Added hypridle.

--- home/tomwrw/features/wm/niri/default.nix
+++ home/tomwrw/features/wm/niri/default.nix
@@ -9,11 +9,35 @@
     ./binds.nix
     ./rules.nix
 
+    ../hypridle.nix
     ../hyprlock.nix
     ../hyprpaper.nix
     ../wofi.nix
     ../waybar.nix
   ];
+
+  xdg.configFile."electron-flags.conf".text = ''
+    --enable-features=UseOzonePlatform
+    --ozone-platform=wayland
+  '';
+
+  xdg.portal = {
+    enable = true;
+    config = {
+      common = {
+        default = ["gtk" "gnome" "gnome-keyring"];
+      };
+      niri = {
+        default = ["gtk" "gnome" "gnome-keyring"];
+      };
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+      gnome-keyring
+    ];
+    xdgOpenUsePortal = true;
+  };
 
   home = {
     packages = with pkgs; [
