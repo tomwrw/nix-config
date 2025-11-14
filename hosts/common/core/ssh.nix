02b17c93e4f67f9feee2e1563ab1edd1527fbcc6
tomwrw <tomwrw@proton.me>
2025-11-14 08:59:03 +0000

    Simplify ssh config.

--- hosts/common/core/ssh.nix
+++ hosts/common/core/ssh.nix
@@ -1,20 +1,10 @@
-{
-  outputs,
-  lib,
-  config,
-  ...
-}: let
-  hosts = lib.attrNames outputs.nixosConfigurations;
+{config, ...}: let
   hostname = config.networking.hostName;
-  domain = config.networking.domain;
-  # Sops needs acess to the keys before the persist dirs are even mounted; so
-  # just persisting the keys won't work, we must point at /persist.
-  hasOptinPersistence = config.environment.persistence ? "/persist";
 in {
   services.openssh = {
     enable = true;
     settings = {
-      # Harden.
+      # Harden ssh.
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
@@ -26,30 +16,16 @@ in {
       AcceptEnv = "WAYLAND_DISPLAY";
       X11Forwarding = true;
     };
-
+    # Format and path for the auto-generated host
+    # ssh keys. I like to use the hostname as a comment.
     hostKeys = [
       {
-        path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
+        path = "/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
         comment = hostname;
       }
     ];
   };
-
-  programs.ssh = {
-    # Each hosts public key.
-    knownHosts = lib.genAttrs hosts (hostname: {
-      publicKeyFile = ../../nixos/${hostname}/keys/id_ed25519_${hostname}.pub;
-      extraHostNames =
-        [
-          "${hostname}.${domain}"
-        ]
-        ++
-        # Alias for localhost if it's the same host.
-        (lib.optional (hostname == config.networking.hostName) "localhost");
-    });
-  };
-
   # Passwordless sudo when SSH'ing with keys.
   security.pam.sshAgentAuth = {
     enable = true;
