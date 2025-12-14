cac8661279b7c2bfaf3cad30b57da537008ac99c
tomwrw <tomwrw@proton.me>
2025-12-14 21:25:13 +0000

    fix: Optimised font packages module to only include the specific nerdfonts I need instead of entire package.

--- hosts/common/core/packages.nix
+++ hosts/common/core/packages.nix
@@ -22,20 +22,20 @@
     unzip
   ];
   # Fonts installed on all hosts go here.
-  fonts.packages = with pkgs;
-    [
-      dejavu_fonts
-      fira-code
-      hack-font
-      ibm-plex
-      inconsolata
-      jetbrains-mono
-      liberation_ttf
-      noto-fonts
-      roboto
-      roboto-mono
-      source-code-pro
-      ttf_bitstream_vera
-    ]
-    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+  fonts.packages = with pkgs; [
+    dejavu_fonts
+    fira-code
+    hack-font
+    ibm-plex
+    inconsolata
+    jetbrains-mono
+    liberation_ttf
+    nerd-fonts.dejavu-sans-mono
+    nerd-fonts.caskaydia-mono
+    noto-fonts
+    roboto
+    roboto-mono
+    source-code-pro
+    ttf_bitstream_vera
+  ];
 }
