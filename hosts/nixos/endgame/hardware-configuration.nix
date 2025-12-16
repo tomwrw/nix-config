3459ed0b2c8b58959c6e52071c01450a29e6222d
tomwrw <tomwrw@proton.me>
2025-12-16 10:16:47 +0000

    fix: Removed networking bridge from endgame due to breaking spicetify and networking.

--- hosts/nixos/endgame/hardware-configuration.nix
+++ hosts/nixos/endgame/hardware-configuration.nix
@@ -21,13 +21,7 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.bridges = {
-    "br0" = {
-      interfaces = ["enp8s0"];
-    };
-  };
-  networking.interfaces."enp8s0".useDHCP = lib.mkDefault false;
-  networking.interfaces."br0".useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
